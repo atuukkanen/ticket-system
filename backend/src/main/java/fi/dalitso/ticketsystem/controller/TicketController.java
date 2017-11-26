c05e6c5c3389716ffc72ff77d5ba6fd1482e5cd5
Aaro Tuukkanen <aaro.tuukkanen@student.tut.fi>
2017-11-26 07:40:03 -0800

    Basis for the backend.

---
+++ backend/src/main/java/fi/dalitso/ticketsystem/controller/TicketController.java
@@ -0,0 +1,29 @@
+package fi.dalitso.ticketsystem.controller;
+
+import fi.dalitso.ticketsystem.domain.Ticket;
+import fi.dalitso.ticketsystem.service.TicketService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TicketController {
+
+    private TicketService ticketService;
+
+    @RequestMapping("/")
+    public List<Ticket> getAllTickets() {
+        return ticketService.getAllTickets();
+    }
+
+    public TicketService getTicketService() {
+        return ticketService;
+    }
+
+    @Autowired
+    public void setTicketService(TicketService ticketService) {
+        this.ticketService = ticketService;
+    }
+}
---
+++ backend/src/main/java/fi/dalitso/ticketsystem/domain/Ticket.java
@@ -0,0 +1,33 @@
+package fi.dalitso.ticketsystem.domain;
+
+
+public class Ticket {
+
+    private Long ticketId;
+    private String header;
+    private String description;
+
+    public Long getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(Long ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
