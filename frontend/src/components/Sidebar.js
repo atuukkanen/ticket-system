ec965f1eec60d6c285f451dc40c3fb86614903e3
Ville Nupponen <ville.nupponen@student.tut.fi>
2017-12-01 14:04:00 +0200

    Make sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import SidebarLink from './SidebarLink'
 import '../styles/Sidebar.css';
 
 class Sidebar extends Component {
     render() {
         return (
-            <div className="sidebar">
-                <ul>
-                    <li>asd</li>
-                    <li>dsa</li>
-                </ul>
+            <div className="nav-side-menu">
+                <div className="brand">
+                    Ticket System
+                </div>
+
+                <div className="menu-list">
+                    <ul id="menu-content" className="menu-content">
+                        <SidebarLink name="dashboard" text="Työpöytä" />
+                        <SidebarLink name="ticket" text="Tiketit" />
+                        <SidebarLink name="users" text="Käyttäjät" />
+                        <SidebarLink name="cog" text="Asetukset" />
+                    </ul>
+                </div>
             </div>
         );
     }
