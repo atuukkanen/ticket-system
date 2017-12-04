2afa0a30275a24e553d5188cef81fe06cbce650e
Ville Nupponen <aikain@gosu.fi>
2017-12-04 23:15:49 +0200

    Make userlist

--- frontend/src/containers/UserList.js
+++ frontend/src/containers/UserList.js
@@ -3,9 +3,52 @@ import '../styles/UserList.css';
 
 class UserList extends Component {
     render() {
+        var users = [{
+            id: 1,
+            username: "aikain",
+            name: "Ville Nupponen"
+        },{
+            id: 2,
+            username: "teppo",
+            name: "Seppo Taalasmaa"
+        },{
+            id: 3,
+            username: "seppo",
+            name: "Teppo Taalasmaa"
+        },{
+            id: 4,
+            username: "pilvi",
+            name: "Pilvi Yksisarvinen"
+        },{
+            id: 5,
+            username: "artzi",
+            name: "Aaro Tuukkanen"
+        }];
         return (
             <div className="userList">
-                Käyttäjät
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h4>Käyttäjät</h4>
+                    </div>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Käyttäjätunnus</th>
+                                <th>Nimi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map(function (user) {
+                                return <tr key={user.id}>
+                                    <td>{user.id}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.name}</td>
+                                </tr>
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
