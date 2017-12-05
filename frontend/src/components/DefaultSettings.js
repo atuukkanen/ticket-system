5d679499b68b1b0a17a88528022c5f140f46d4b3
Ville Nupponen <aikain@gosu.fi>
2017-12-05 12:40:53 +0200

    Add top navigation bar to settings view

---
+++ frontend/src/components/DefaultSettings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DefaultSettings extends Component {
+    render() {
+        return (
+            <div>
+                oletus
+            </div>
+        );
+    }
+}
+
+export default DefaultSettings;
---
+++ frontend/src/components/LdapSettings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LdapSettings extends Component {
+    render() {
+        return (
+            <div>
+                ldap
+            </div>
+        );
+    }
+}
+
+export default LdapSettings;
