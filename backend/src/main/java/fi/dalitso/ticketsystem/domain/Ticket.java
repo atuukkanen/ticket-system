a2c186b433ddcfe7be2d8968cb55b717e988bea7
Aaro Tuukkanen <aaro.tuukkanen@student.tut.fi>
2017-12-10 10:46:16 -0800

    Added status attribute for ticket. Added more methods for ticket handling.

--- backend/src/main/java/fi/dalitso/ticketsystem/domain/Ticket.java
+++ backend/src/main/java/fi/dalitso/ticketsystem/domain/Ticket.java
@@ -10,6 +10,15 @@ public class Ticket extends AbstractPersistable<Long> {
 
     private String header;
     private String description;
+    private Status status;
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 
     public String getHeader() {
         return header;
@@ -25,5 +34,16 @@ public class Ticket extends AbstractPersistable<Long> {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void update(Ticket uTicket) {
+        if (uTicket.getHeader() != null)
+            setHeader(uTicket.getHeader());
+
+        if (uTicket.getDescription() != null)
+            setDescription(uTicket.getDescription());
+    
+        if (uTicket.getStatus() != null)
+            setStatus(uTicket.getStatus());
     }
 }
--- backend/src/main/java/fi/dalitso/ticketsystem/repository/TicketRepository.java
+++ backend/src/main/java/fi/dalitso/ticketsystem/repository/TicketRepository.java
@@ -1,7 +1,11 @@
 package fi.dalitso.ticketsystem.repository;
 
+import fi.dalitso.ticketsystem.domain.Status;
 import fi.dalitso.ticketsystem.domain.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    public List<Ticket> findAllByStatus(Status status);
 }
