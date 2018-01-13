4eba77207acc36c80c56b3f6bfceb9091a0fd5bd
Ville Nupponen <aikain@gosu.fi>
2018-01-13 21:20:48 +0200

    Fetch ticket data from backend. Fix form-row labels and inputs width. Add link to new ticket creating.

--- frontend/src/components/FormSubmitRow.js
+++ frontend/src/components/FormSubmitRow.js
@@ -7,9 +7,9 @@ class FormSubmitRow extends Component {
     render() {
         return (
             <div className="form-group">
-                <label className="col-md-3 control-label"/>
-                <div className="col-md-8">
-                    <input type="button" className="btn btn-primary" value="Tallenna" />
+                <label className="col-md-2 control-label"/>
+                <div className="col-md-9">
+                    <input type="submit" className="btn btn-primary" value="Tallenna" />
                     <span/>
                     <input type="reset" className="btn btn-default" value="Peruuta" onClick={() => this.backToPrevPage()} />
                 </div>
