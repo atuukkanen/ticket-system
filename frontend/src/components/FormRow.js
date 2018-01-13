a7ef7cdd0e0ad82cfe19e95c43d0e0d68940d2d2
Ville Nupponen <aikain@gosu.fi>
2018-01-13 21:14:37 +0200

    Add template and required-attr to FormRow

--- frontend/src/components/FormRow.js
+++ frontend/src/components/FormRow.js
@@ -6,7 +6,17 @@ class FormRow extends Component {
             <div className="form-group">
                 <label className="col-md-2 control-label">{ this.props.text }:</label>
                 <div className="col-md-9">
-                    <input className="form-control" type="text" name={ this.props.name } value={ this.props.value } onChange={this.props.onChange} />
+                    {
+                        this.props.template !== undefined ? this.props.template : (
+                            <input
+                                className="form-control"
+                                type={ this.props.type !== undefined ? this.props.type : "text" }
+                                name={ this.props.name }
+                                value={ this.props.value }
+                                onChange={ this.props.onChange }
+                                required={ this.props.required !== undefined ? this.props.required : false } />
+                        )
+                    }
                 </div>
             </div>
         );
