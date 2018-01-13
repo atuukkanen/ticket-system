9fd9f8b4111661320d2cfd179710290b0b51bed7
Aaro Tuukkanen <aaro.tuukkanen@dalitso.fi>
2018-01-13 13:29:44 +0200

    Added basic email sending functionality.

---
+++ backend/src/main/java/fi/dalitso/ticketsystem/service/EmailService.java
@@ -0,0 +1,25 @@
+package fi.dalitso.ticketsystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private JavaMailSender mailSender;
+
+    public void sendMessage(String to, String subject, String content) {
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(content);
+        mailSender.send(simpleMailMessage);
+    }
+
+    @Autowired
+    public void setMailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+}
