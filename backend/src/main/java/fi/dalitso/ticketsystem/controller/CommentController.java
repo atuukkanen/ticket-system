6f08cbb1905a808c0dba0212178fcd1d08d22e7e
Aaro Tuukkanen <aaro.tuukkanen@dalitso.fi>
2018-01-14 13:39:26 +0200

    Added missing @RequestBody annotations.

--- backend/src/main/java/fi/dalitso/ticketsystem/controller/CommentController.java
+++ backend/src/main/java/fi/dalitso/ticketsystem/controller/CommentController.java
@@ -5,10 +5,7 @@ import fi.dalitso.ticketsystem.domain.ModificationInfo;
 import fi.dalitso.ticketsystem.domain.Ticket;
 import fi.dalitso.ticketsystem.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,14 +22,14 @@ public class CommentController {
     }
 
     @RequestMapping(value = "/{commentId}", method = RequestMethod.PUT)
-    public Comment update(@PathVariable Long commentId, Comment comment) {
+    public Comment update(@PathVariable Long commentId, @RequestBody Comment comment) {
         // TODO: Get the real editing user.
         comment.setCreator(new ModificationInfo("Editing user"));
         return commentService.update(commentId, comment);
     }
 
     @RequestMapping(value = "/{ticketId}", method = RequestMethod.POST)
-    public Ticket addNewComment(@PathVariable Long ticketId, Comment comment) {
+    public Ticket addNewComment(@PathVariable Long ticketId, @RequestBody Comment comment) {
         // TODO: Get the real commenting user.
         comment.setCreator(new ModificationInfo("Commenting user"));
         return commentService.addNewComment(ticketId, comment);
