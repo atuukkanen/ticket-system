1ca20e4221e3e1df78dc39ae4ea417eb7ac9fe8d
Aaro Tuukkanen <aaro.tuukkanen@student.tut.fi>
2018-01-14 20:12:39 +0200

    Added functionality for changing mail server on runtime.

--- backend/README.md
+++ backend/README.md
@@ -3,10 +3,12 @@
 
 ## Environmental Variables
 
-### Required
+### Optional, but required for mail notifications to work "out-of-the-box"
 * MAIL_HOSTNAME (example: ```smtp.emailserver.com```)
 * MAIL_USERNAME (example: ```ticketsystememailuser```)
 * MAIL_PASSWORD (example: ```XQ8b0nz!^QwU0}bdW$sXZAEiM1R4^O+>G-NZ~%6/}7.[@uW&AHn~Y$/A[[g`se[.```)
+
+### Required
 * DATABASE_URL (example: ```jdbc:mysql://databasehost.com:3306/ticketdatabase```)
 * DATABASE_USERNAME (example: ```ticketuser```)
 * DATABASE_PASSWORD (example: ```pBiJ%8Wa%]<Hl_#'tCO9I{<6fm[]P_rcIT'F+;&JjJtYJ~&z-o.^mm=!?:Y-l.=/```)
@@ -24,3 +26,24 @@ Swagger UI documentation for endpoints is available in host/application-root/swa
 ## Mail Templates
 
 Mail templates use Thymeleaf. Mail template options are defined in resources/mailtemplate.properties.
+
+## Configuring Mail On Runtime
+
+Endpoint ```/settings/mail``` provides functionality for configuring mail on runtime. Example request body for setting a new mail sending server:
+```
+{
+    "mailSenderStuff" : {
+        "MAIL_HOSTNAME" : "smtp.gmail.com",
+        "MAIL_PORT" : "587",
+        "MAIL_PROTOCOL" : "smtp",
+        "MAIL_USERNAME" : "ticketusername@gmail.com",
+        "MAIL_PASSWORD" : "app-password"
+    },
+    "mailProperties" : {
+        "mail.smtp.auth" : "true",
+        "mail.smtp.starttls.enable" : "true",
+        "mail.smtp.ssl.trust" : "smtp.gmail.com",
+        "mail.smtp.quitwait" : "false"
+    }
+}
+```
--- backend/src/main/java/fi/dalitso/ticketsystem/configuration/MailConfig.java
+++ backend/src/main/java/fi/dalitso/ticketsystem/configuration/MailConfig.java
@@ -10,6 +10,8 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import static fi.dalitso.ticketsystem.configuration.EnvironmentTool.getEnvironmentVariable;
@@ -22,28 +24,72 @@ public class MailConfig {
     private Environment environment;
 
     @Bean
-    public JavaMailSender mailSender() throws IOException {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(
+    public JavaMailSender mailSender() {
+        try {
+            return generateDefaultMailSender();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private JavaMailSender generateDefaultMailSender()
+            throws IOException, RuntimeException {
+        HashMap<String, String> mailSenderStuff = new HashMap<>();
+        HashMap<String, String> mailProperties = new HashMap<>();
+
+        mailSenderStuff.put("MAIL_HOSTNAME",
                 getEnvironmentVariable("MAIL_HOSTNAME"));
-        mailSender.setPort(
-                Integer.parseInt(getEnvironmentVariable("MAIL_PORT", "587")));
-        mailSender.setProtocol(
+        mailSenderStuff.put("MAIL_PORT",
+                getEnvironmentVariable("MAIL_PORT", "587"));
+        mailSenderStuff.put("MAIL_PROTOCOL",
                 getEnvironmentVariable("MAIL_PROTOCOL", "smtp"));
-        mailSender.setUsername(
+        mailSenderStuff.put("MAIL_USERNAME",
                 getEnvironmentVariable("MAIL_USERNAME"));
-        mailSender.setPassword(
+        mailSenderStuff.put("MAIL_PASSWORD",
                 getEnvironmentVariable("MAIL_PASSWORD"));
+
+        mailProperties.put("mail.smtp.auth",
+                environment.getRequiredProperty("mail.smtp.auth"));
+        mailProperties.put("mail.smtp.starttls.enable",
+                environment.getRequiredProperty("mail.smtp.starttls.enable"));
+        mailProperties.put("mail.smtp.ssl.trust",
+                getEnvironmentVariable("MAIL_HOSTNAME"));
+        mailProperties.put("mail.smtp.quitwait",
+                environment.getRequiredProperty("mail.smtp.quitwait"));
+
+        return generateMailSender(mailSenderStuff, mailProperties);
+    }
+
+    public static JavaMailSender generateMailSender(
+            Map<String, String> mailSenderStuff,
+            Map<String, String> mailProperties) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost(
+                mailSenderStuff.get("MAIL_HOSTNAME"));
+
+        String portStr = mailSenderStuff.get("MAIL_PORT");
+        if (portStr == null)
+            portStr = "587";
+        mailSender.setPort(Integer.parseInt(portStr));
+
+        String protocol = mailSenderStuff.get("MAIL_PROTOCOL");
+        if (protocol == null)
+            protocol = "smtp";
+        mailSender.setProtocol(protocol);
+
+        mailSender.setUsername(mailSenderStuff.get("MAIL_USERNAME"));
+        mailSender.setPassword(mailSenderStuff.get("MAIL_PASSWORD"));
 
         Properties javaMailProperties = new Properties();
         javaMailProperties.put("mail.smtp.auth",
-                environment.getRequiredProperty("mail.smtp.auth"));
+                mailProperties.get("mail.smtp.auth"));
         javaMailProperties.put("mail.smtp.starttls.enable",
-                environment.getRequiredProperty("mail.smtp.starttls.enable"));
+                mailProperties.get("mail.smtp.starttls.enable"));
         javaMailProperties.put("mail.smtp.ssl.trust",
                 getEnvironmentVariable("MAIL_HOSTNAME"));
         javaMailProperties.put("mail.smtp.quitwait",
-                environment.getRequiredProperty("mail.smtp.quitwait"));
+                mailProperties.get("mail.smtp.quitwait"));
         mailSender.setJavaMailProperties(javaMailProperties);
         return mailSender;
     }
