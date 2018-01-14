128d4942c7f39e858de2e9d00f7617e6a0f3e547
Ville Nupponen <aikain@gosu.fi>
2018-01-14 20:50:07 +0200

    Update bootstrap to 4.0.0-alpha.6, fix margins

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import 'font-awesome/css/font-awesome.min.css';
 import './styles/index.css';
 import App from './containers/App';
